f1718c54f9adff029768c44185ec6b4a77f18f29
Talal Zafar <talalzafar84@gmail.com>
2025-03-17 13:18:33 +0500

    .

--- src/pages/CalculatePrice/index.jsx
+++ src/pages/CalculatePrice/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-// import Expenditure from "../../components/Expenditure";
 import Build from "../../components/Build";
 
 const CalculatePrice = () => {
   return (
     <main>
       <Build />
-      {/* <Expenditure /> */}
     </main>
   );
 };
